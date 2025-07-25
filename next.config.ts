32ac4e3aa8f4f899425906fa00d24a7aa8ccfe48
arththakkar1 <abhithakkar466@gmail.com>
2025-07-26 00:29:16 +0530

    Added my project files

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
- images: {
-    domains: ['images.pexels.com'], 
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    domains: ["images.pexels.com"],
   },
 };
 
